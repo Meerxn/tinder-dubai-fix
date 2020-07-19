9c7778ab11d43e4245c4718ed8cb940d6a3944fb
Fardeen Meeran <61164969+Meerxn@users.noreply.github.com>
2020-07-19 12:25:50 +0400

    Update main.js
    main requirement comments added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var nope = document.querySelector('*[aria-label^="Nope"]');
  var yes = document.querySelector('*[aria-label^="Like"]');
  var yesCounter = 0 ; 
-
+// Need to make black list 
+// Ell has two profiles so need to index correctly 
+// also use backboard class as that has bio which sometimes has numbers too. 
 
 for ( i = 0 ; i < document.querySelectorAll(".Ell").length ; i++){
     if(document.querySelectorAll(".Ell")[i].innerHTML.includes("Ws")){
